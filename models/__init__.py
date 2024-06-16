00d2b2f5080aab443a8789f666574dbe1fc287e1
krish <ksnjsi@gmail.com>
2024-06-16 18:35:48 +0530

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+#this python package  includes all processing functions for video processing and detection
+#all the required functions
+
+from .gender_classification import gender_model
+from .indoor_outdoor_classification import indoor_outdoor_model
+
+if __name__ == "__main__":
+    print("hello")
